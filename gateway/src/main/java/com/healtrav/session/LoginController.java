309d67e2bcf5629a0de66ebd2690294ec84d4348
yi.cui <yi.cui@shdanyan.com>
2016-10-17 00:21:25 +0800

    remove @CrossOrigin

--- gateway/src/main/java/com/healtrav/session/LoginController.java
+++ gateway/src/main/java/com/healtrav/session/LoginController.java
@@ -1,14 +1,12 @@
 package com.healtrav.session;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    @CrossOrigin(origins = "*", maxAge = 3600)
     public String login() {
         return "forward:/";
     }
